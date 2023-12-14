f368aeb790c16db89f4a5e3c2e79d2b57a8f1be8
jvanbusselall <jaap.van.bussel@alliander.com>
2023-12-14 11:01:35 +0100

    opdracht web app

---
+++ sw.js
@@ -0,0 +1,42 @@
+self.addEventListener('fetch', event => {
+    let url = 'https://bgg-json.azurewebsites.net/collection/edwalter';
+    fetch(url).then(response => {
+            return response.json();
+        }
+    )
+});
+
+self.addEventListener('fetch', (ev) => {
+    ev.respondWith(
+
+        // Deze functie wilt een Promise;
+        // 'async function(){..}() wordt door JavaScript
+        // AUTOMATISCH in een Promise verpakt! :-)
+
+        async function() {
+
+            console.log(`Request onderschept met data: `);
+            console.dir(ev.request);
+
+            if (ev.request.url.indexOf('bgg')>-1){
+
+                let oudRequest = ev.request;
+                let nieuweURL = oudRequest.url.replace('edwalter','stinow');
+
+                let nieuwRequest = new Request(
+                    nieuweURL,
+                    {
+                        method: oudRequest.method,
+                        headers: oudRequest.headers,
+                        mode: oudRequest.mode,
+                        credentials: oudRequest.credentials
+                    }
+                );
+
+                return fetch(nieuwRequest); //Hier wordt een Promise verwacht
+            } else {
+                return fetch(ev.request);
+            }
+        }()
+    );
+});
